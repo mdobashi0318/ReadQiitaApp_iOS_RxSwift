12929dd56e4ae1a5e40a744fcdc0be0dc13a8cff
MasaharuDobashi <masaharu.dobashi@gmail.com>
2023-07-07 12:22:29 +0900

    APIManagerの変更

--- ReadQiitaApp_iOS_RxSwift/Other/APIManager.swift
+++ ReadQiitaApp_iOS_RxSwift/Other/APIManager.swift
@@ -6,41 +6,35 @@
 //
 
 import Foundation
-import RxSwift
-import RxCocoa
+import Alamofire
 
 
 
 struct APIManager {
     
     private static let baseUrl = "https://qiita.com/api/v2/"
-    
-    private static let disposeBag = DisposeBag()
-    
+        
     static func get<T: Codable>(request: String, success: @escaping (T) -> Void, failure: @escaping (APIError) -> Void)  {
-        
-        let session = URLSession.shared
-        
         guard let url = URL(string: baseUrl + request) else {
             failure(APIError.init(message: "url Error"))
             return
         }
         
-        session.rx.response(request: URLRequest(url: url))
-            .subscribe { response, data in
-                do {
-                    guard 200 ..< 300 ~= response.statusCode else {
-                        throw APIError.init(message: "statusCode: \(response.statusCode)")
-                    }
-                    let articles = try JSONDecoder().decode(T.self, from: data)
-                    success(articles)
-                } catch {
-                    failure(APIError.init(message: error.localizedDescription))
-                }
-            }.disposed(by: disposeBag)
+        AF.request(url)
+            .validate()
+            .responseData { result in
+            
+            do {
+                let articles = try JSONDecoder().decode(T.self, from: result.data!)
+                success(articles)
+            } catch {
+                failure(APIError.init(message: error.localizedDescription))
+            }
+            
+        }
         
     }
-
+    
 }
 
 
